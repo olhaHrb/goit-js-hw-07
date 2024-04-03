d1d747046d29aa39c8a04860fa11d8174f11a199
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-04-03 21:46:15 +0300

    task6 error correction

--- js/task-6.js
+++ js/task-6.js
@@ -13,31 +13,38 @@ let width = 30;
 let height = 30;
 
 
+
 createBtn.addEventListener("click", handleCreate);
+
 function handleCreate(event) {
   if (input.value < 1 || input.value > 100) {
     return;
   } 
+
   createBoxes(input.value);
 }
 
 function createBoxes() {
   divParent.innerHTML = '';
+  const boxes = [];
+
   
   for (let i = 0; i < input.value; i += 1)  {
     const box = document.createElement("div");
-    divParent.append(box);
     box.style.width = `${width}px`;
     box.style.height = `${height}px`;
     box.style.backgroundColor = getRandomHexColor();
     width += 10;
     height += 10;
+    boxes.push(box);
   }
 
   input.value = "";
   width = 30;
   height = 30;
+  divParent.append(...boxes);
 }
+
 
 
 destroyBtn.addEventListener("click", handleDestroy);
